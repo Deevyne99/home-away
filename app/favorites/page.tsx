54a7049148ff60e9c452b38b0c350ac3271d0c4f
Deevyne99 <kaludivine545@gmail.com>
2024-10-16 14:26:16 +0100

    first commit

---
+++ app/favorites/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FavouritesPage = () => {
+  return <div>Facourites Page</div>
+}
+
+export default FavouritesPage
---
+++ app/rentals/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RentalsPage = () => {
+  return <div>Rentals page</div>
+}
+
+export default RentalsPage
